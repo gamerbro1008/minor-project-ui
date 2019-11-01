a393fbe1ec65b7a827eb6909571eabb73becc3de
Astha Jain <jainastha1008@gmail.com>
2019-11-01 22:23:56 +0530

    init

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,54 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    selectedOrg: null,
+    selectedDept: null,
+    selectedActivities: [{ id: 2 }],
+    selectedPosition: 1
+  },
+  mutations: {
+    selectedOrgChanged(state, newOrg) {
+      state.selectedDept = null;
+      state.selectedOrg = newOrg;
+    },
+    selectedDeptChanged(state, newDept) {
+      state.selectedDept = newDept;
+    },
+    addActivity(state, activity) {
+      state.selectedActivities.splice(state.selectedPosition, 0, activity);
+      state.selectedPosition += 1;
+    },
+    removeActivity(state, index) {
+      state.selectedActivities.splice(index, 1);
+      state.selectedPosition -= 1;
+    },
+    clearActivities(state) {
+      state.selectedActivities = [];
+      state.selectedPosition = 0;
+    },
+    changeSelectedPosition(state, index = null) {
+      if (index == null) {
+        index = state.selectedActivities.length - 1;
+      }
+      state.selectedPosition = index;
+    }
+  },
+  getters: {
+    selectedOrgChanged(state) {
+      return state.selectedOrg;
+    },
+    selectedDeptChanged(state) {
+      return state.selectedDept;
+    },
+    activities(state) {
+      return state.selectedActivities;
+    },
+    lastActivity(state) {
+      return state.selectedActivities[state.selectedActivities.length - 1] || 0;
+    },
+    selectedPosition(state) {
+      return state.selectedPosition;
+    }
+  }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ModelBuilder />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ModelBuilder from "@/components/ModelBuilder/ModelBuilder.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    ModelBuilder
   }
 };
 </script>
